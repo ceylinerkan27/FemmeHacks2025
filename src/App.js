40d64bc7c063ed4ed61bced3f7479c9f2e1fb40a
ziana-sundrani <zianasundrani@gmail.com>
2025-02-08 08:55:03 -0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './App.css';
+import Journal from './Components/UserJournal/Journal';
+function App() {
+  return (
+      <Journal/>
+  );
+}
+
+export default App;
