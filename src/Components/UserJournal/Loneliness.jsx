f419aee4dcc983380823e38da9bb74fef406c434
ceylinerkan27 <ceylin@seas.upenn.edu>
2025-02-08 10:48:27 -0500

    Create Loneliness.jsx

---
+++ src/Components/UserJournal/Loneliness.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+function Loneliness() {
+  return (
+    <div>
+      <h1>Loneliness Resources</h1>
+      <ul>
+        <li>
+          <a
+            href="https://www.psychologytoday.com/us/blog/living-forward/202005/how-deal-loneliness"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            How to Deal with Loneliness
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.meetup.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Join a Group or Community Activity Near You
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.betterhelp.com/advice/loneliness/how-to-deal-with-loneliness/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Tips on Overcoming Loneliness
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Loneliness;
