9f67b614267a7e119044c20c1d7d2819b4f52fc1
ceylinerkan27 <ceylin@seas.upenn.edu>
2025-02-08 10:45:19 -0500

    Create Anxiety.jsx

---
+++ src/Components/UserJournal/Anxiety.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function Anxiety() {
+  return (
+    <div>
+      <h1>Anxiety Resources</h1>
+      <ul>
+        <li>
+          <a href="https://www.anxiety.org" target="_blank" rel="noopener noreferrer">
+            Anxiety Resources
+          </a>
+        </li>
+        <li>
+          <a href="https://www.headspace.com/meditation" target="_blank" rel="noopener noreferrer">
+            Meditation for Calmness
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Anxiety;
