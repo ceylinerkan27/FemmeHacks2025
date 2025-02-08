338687ecff35d9528d03927ffe23dab9486d838d
ceylinerkan27 <ceylin@seas.upenn.edu>
2025-02-08 10:48:00 -0500

    Create Sadness.jsx

---
+++ src/Components/UserJournal/Sadness.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+function Sadness() {
+  return (
+    <div>
+      <h1>Sadness Resources</h1>
+      <ul>
+        <li>
+          <a
+            href="https://www.verywellmind.com/ways-to-cope-with-sadness-5093285"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Ways to Cope with Sadness
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.betterhelp.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Talk to a Therapist Online
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.psychologytoday.com/us/blog/liking-the-child-you-love/201809/5-ways-deal-sadness"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            5 Ways to Deal with Sadness
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Sadness;
