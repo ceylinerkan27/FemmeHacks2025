0743f0a57b1fc3649ac867d0169524ab9b3634cc
ceylinerkan27 <ceylin@seas.upenn.edu>
2025-02-08 10:46:05 -0500

    Create Stress.jsx

---
+++ src/Components/UserJournal/Stress.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function Stress() {
+  return (
+    <div>
+      <h1>Stress Resources</h1>
+      <ul>
+        <li>
+          <a href="https://www.stress.org" target="_blank" rel="noopener noreferrer">
+            Coping with Stress
+          </a>
+        </li>
+        <li>
+          <a href="https://www.mindful.org" target="_blank" rel="noopener noreferrer">
+            Mindful Practices to Reduce Stress
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Stress;
